547e0c2522c86f12118f4628cb0e5162efb899c0
amelies08 <amelie.sarrazin2@gmail.com>
2025-02-14 16:14:59 -0500

    Labo4_VersionFinale

--- BonhommePendu.py
+++ BonhommePendu.py
@@ -2,58 +2,72 @@ lettre1 = lettre2 = lettre3 = lettre4 = "_" #variables pour montrer le nombre de
 
 bonhomme = ["o", "|", "^"] #variables du bonhomme
 
-reponse = "wham"
+reponse = ["wham", "cure"]
 
 print("Devine le groupe de musique")
 mauvais = 0 #mauvaises reponses
-x = False
-z = False
 
-while not x or not z: #tant que guess n'est pas une lettre et qu'il n'y a pas seulement une lettre
-    guess = input() #devine la lettre
-    x = guess.isalpha() #est-ce que guess est une lettre
-    z = len(guess) == 1 #est-ce qu'il y a seulement une lettre
-while (lettre1 != reponse[0] or lettre2 != reponse[1] or lettre3 != reponse[2] or lettre4 != reponse[3]) and mauvais < 3: #pendant qu'il y a au moins un lettre qui n'est pas devine et qu'il y a moins de 3 mauvaises reponses
-    if guess == reponse[0]: #si la lettre devinee est une des lettres du mot
-        lettre1 = reponse[0] #remplace la ligne par la lettre au bon emplacement
-        print("bonne reponse")
-    elif guess == reponse[1]:
-        lettre2 = reponse[1]
-        print("bonne reponse")
-    elif guess == reponse[2]:
-        lettre3 = reponse[2]
-        print("bonne reponse")
-    elif guess == reponse[3]:
-        lettre4 = reponse[3]
-        print("bonne reponse")
-    else:
-        mauvais = mauvais + 1 #changer le nombre de mauvaises reponse lorsque necessaire et inscrire ce nombre
-        print("mauvaise reponse")
-        if mauvais == 1:
-            print(bonhomme[0]) #Ajouter une partie du bonhomme
-            for y in range(1): #range et y sont egales a 0
-                if y == 0:
-                    print("C'est un groupe des années 80.") #montrer premier indice
-                    continue
+for mot in reponse:
+    while (lettre1 != mot[0] or lettre2 != mot[1] or lettre3 != mot[2] or lettre4 != mot[3]) and mauvais < 3: #pendant qu'il y a au moins un lettre qui n'est pas devine et qu'il y a moins de 3 mauvaises reponses
+        x = False
+        z = False
+        while not x or not z: #tant que guess n'est pas une lettre et qu'il n'y a pas seulement une lettre
+            guess = input() #devine la lettre
+            x = guess.isalpha() #est-ce que guess est une lettre
+            z = len(guess) == 1 #est-ce qu'il y a seulement une lettre
+        if guess == mot[0]: #si la lettre devinee est une des lettres du mot
+            lettre1 = mot[0] #remplace la ligne par la lettre au bon emplacement
+            print("bonne reponse")
+        elif guess == mot[1]:
+            lettre2 = mot[1]
+            print("bonne reponse")
+        elif guess == mot[2]:
+            lettre3 = mot[2]
+            print("bonne reponse")
+        elif guess == mot[3]:
+            lettre4 = mot[3]
+            print("bonne reponse")
         else:
-            print(bonhomme[0]) #ajouter une autre partie du bohomme
+            mauvais = mauvais + 1 #changer le nombre de mauvaises reponse lorsque necessaire et inscrire ce nombre
+            print("mauvaise reponse")
+            if mauvais == 1:
+                print(bonhomme[0]) #Ajouter une partie du bonhomme
+                if mot == "wham":
+                    for y in range(1): #range et y sont egales a 0
+                        if y == 0:
+                            print("C'est un groupe des années 80.") #montrer premier indice
+                            continue
+                else:
+                    for y in range(1): #range et y sont egales a 0
+                        if y == 0:
+                            print("C'est un groupe de rock qui a été formé en Angleterre dans les années 70.") #montrer premier indice
+                            continue
+            else:
+                print(bonhomme[0]) #ajouter une autre partie du bohomme
+                print(bonhomme[1])
+                if mot == "wham":
+                    for y in range(1): #range et y sont egales a 0
+                        if y == 0:
+                            print("Un des membres du groupe a fait de la musique solo par la suite.") #montrer premier indice
+                            continue
+                else:
+                    for y in range(1): #range et y sont egales a 0
+                        if y == 0:
+                            print("Le chanteur se nomme Robert Smith.") #montrer premier indice
+                            continue
+        print(lettre1, lettre2, lettre3, lettre4) #ecrire les lettres devoilees et leur emplacements
+    else: #s'il y a 3 mauvaises reponses ou que toutes les reponses sont bonnes
+        if lettre1 == mot[0] and lettre2 == mot[1] and lettre3 == mot[2] and lettre4 == mot[3] and mauvais < 3: #si toutes les lettres sont devoilees et qu'il y a moins de 3 mauvaises reponses
+            print("bravo")
+            continuer = input("Voulez-vous continuer?")
+            if continuer == "oui":
+                lettre1 = lettre2 = lettre3 = lettre4 = "_"
+                print(lettre1, lettre2, lettre3, lettre4)
+                continue
+            elif continuer == "non":
+                break
+        elif mauvais == 3: #s'il y a 3 mauvaises reponses
+            print(bonhomme[0]) #imprime le bonhomme au complet
             print(bonhomme[1])
-            for y in range(1,2): #range et y sontegales a 1
-                if y == 1:
-                    print("Un des deux membres a fait une carrière solo après.") #montrer deuxieme indice
-                    continue
-    print(lettre1, lettre2, lettre3, lettre4) #ecrire les lettres devoilees et leur emplacements
-    x = False
-    z = False
-    while not x or not z:
-        guess = input()
-        x = guess.isalpha()
-        z = len(guess) == 1
-else: #s'il y a 3 mauvaises reponses ou que toutes les reponses sont bonnes
-    if lettre1 == reponse[0] and lettre2 == reponse[1] and lettre3 == reponse[2] and lettre4 == reponse[3] and mauvais < 3: #si toutes les lettres sont devoilees et qu'il y a moins de 3 mauvaises reponses
-        print("bravo")
-    elif mauvais == 3: #s'il y a 3 mauvaises reponses
-        print(bonhomme[0]) #imprime le bonhomme au complet
-        print(bonhomme[1])
-        print(bonhomme[2])
-        print("game over")
+            print(bonhomme[2])
+            print("game over")
--- modifications.py
+++ modifications.py
@@ -1,3 +1,4 @@
 """J'ai ajouté des comentiares pour améliorer la documentatio du code.
 J'ai modifié le code pour que la réponse soit de seulement une lettre.
-J'ai changé mes variables pour que ça soit plus court et plus simple."""
+J'ai changé mes variables pour que ça soit plus court et plus simple.
+J'ai ajouter un deuxième mot à deviner et une option pour continuer au deuxième mot ou pour arrêter le jeu."""
